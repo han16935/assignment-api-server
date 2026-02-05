eef4c5a36806af8c9d7341e81ae13dbc198bae88
han16935 <han1693516@gmail.com>
2026-02-05 19:01:33 +0900

    first commit

---
+++ src/test/kotlin/com/ocr/api_server/ApiServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ocr.api_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
